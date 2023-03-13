3c77876a4643cbe0475c2c4b3be3960d434576e8
Muhanned Alsoradi <muhannedalsoradi@gmail.com>
2023-03-13 23:50:22 +0200

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,12 +1,8 @@
 def bubble_sort(arr):
     n = len(arr)
     type = input('Enter the type of sorting [asc,desc,none]: ')
-    # Traverse through all array elements
     for i in range(n):
-        # Last i elements are already in place
         for j in range(0, n-i-1):
-            # Traverse the array from 0 to n-i-1
-            # Swap if the element found is greater than the next element
             if type == 'asc':
                 if arr[j] > arr[j+1]:
                     arr[j], arr[j+1] = arr[j+1], arr[j]
@@ -19,4 +15,4 @@ def bubble_sort(arr):
 
 arr = [64, 34, 25, 12, 22, 11, 90]
 sorted_arr = bubble_sort(arr)
-print(sorted_arr)  # Output: [11, 12, 22, 25, 34, 64, 90]
+print(sorted_arr)
